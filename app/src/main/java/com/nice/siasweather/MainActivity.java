89bc72a0cabaadd897beb719b576e998780bdffc
nmm <157063893@qq.com>
2017-07-01 16:11:28 +0800

    加入显示城市天气信息

--- app/src/main/java/com/nice/siasweather/MainActivity.java
+++ app/src/main/java/com/nice/siasweather/MainActivity.java
@@ -1,13 +1,25 @@
 package com.nice.siasweather;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Created by 萌 on 2017/7/1.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getString("weather", null) != null) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
